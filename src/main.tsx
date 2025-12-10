b1a23f1c40f452a0bb3b502c469fb1dd13a33b75
Sandlo <luca-burghardt@gmx.de>
2025-12-10 15:50:59 +0100

    Change ts config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 
   import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
+  import App from "./App";
   import "./index.css";
+  import React from "react";
 
   createRoot(document.getElementById("root")!).render(<App />);
   
